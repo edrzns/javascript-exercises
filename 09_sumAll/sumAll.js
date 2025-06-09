1c33265579bc7dd04898edba87e3288ec65d4ebf
Edijs Rozens <edijs.rozens@gmail.com>
2025-06-09 19:53:27 +0000

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0) {
+        return 'ERROR';
+    };
 
+    let sum = 0
+
+    let min = Math.min(x, y);
+    let max = Math.max(x, y);
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
