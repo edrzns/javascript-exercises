fc5b4aeed3905d9c22dbd812d47696d673c25e62
Edijs Rozens <edijs.rozens@gmail.com>
2025-06-09 20:44:26 +0000

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return result = x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return result = x - y;
 };
 
-const sum = function() {
-	
+const sum = function(n) {
+	let total = 0;
+  for (let i = 0; i < n.length; i++) {
+    total += n[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  return num.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return result = x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) return undefined; // no factorial for negative numbers
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
