a8ccc3718d3df466b31c37889cc0f9c7dbd5cfb2
Edijs Rozens <edijs.rozens@gmail.com>
2025-06-10 08:44:19 +0000

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleaned = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reverse = cleaned.split("").reverse("").join("");
 
+    return cleaned === reverse;
 };
 
 // Do not edit below this line
