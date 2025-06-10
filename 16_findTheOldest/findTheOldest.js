b0fb269110b1c71deef38ec502e1ea483f9ce813
Edijs Rozens <edijs.rozens@gmail.com>
2025-06-10 19:28:42 +0000

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        const getAge = (p) => (p.yearOfDeath || new Date().getFullYear()) - p.yearOfBirth;
+        return getAge(person) > getAge(oldest) ? person : oldest;
+    });
 };
 
 // Do not edit below this line
